664a0966b1013450fb2436e3f44ecb74bdaa13d0
bai ngay7_8 <tongngoctuan09@gmail.com>
2024-09-06 14:35:43 +0700

    bAI -5

---
+++ Baitap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,26 @@
+﻿using Baitap05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngay05";
+            ViewBag.Thang = "Thang08";
+            ViewData["Nam"] = "Nam2004";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var TheLoai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "Tong Ngoc Tuan"
+
+            };
+            return View(TheLoai);
+        }
+    }
+}
