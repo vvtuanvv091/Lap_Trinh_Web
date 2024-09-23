8119252199d68a1ef7d13f96bc62da47e848a600
bai ngay7_8 <tongngoctuan09@gmail.com>
2024-09-23 15:09:52 +0700

    baimoi

---
+++ projecta/Models/SanPham.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace projecta.Models
+{
+    public class SanPham
+    {
+            [Key]
+            public int Id { get; set; }
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public double price { get; set; }
+            public string? Description { get; set; }
+            public string? ImageUrl { get; set; }
+            [Required]
+            public int TheLoaiId { get; set; }
+            [ForeignKey("TheLoaiId")]
+            public TheLoaiViewModel TheLoai { get; set; }
+        
+    }
+}
