d205d82743b6a88313384bf133b65d1285436a61
bai ngay7_8 <tongngoctuan09@gmail.com>
2024-09-24 15:31:23 +0700

    add

---
+++ BaiKiemTra02/BaiKiemTra02/Data/Migrations/20240924082356_hoc.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BaiKiemTra02.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class hoc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateOut",
+                table: "LopHoc",
+                newName: "ngayratruong");
+
+            migrationBuilder.RenameColumn(
+                name: "DateCreated",
+                table: "LopHoc",
+                newName: "ngaynhaptruong");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ngayratruong",
+                table: "LopHoc",
+                newName: "DateOut");
+
+            migrationBuilder.RenameColumn(
+                name: "ngaynhaptruong",
+                table: "LopHoc",
+                newName: "DateCreated");
+        }
+    }
+}
---
+++ BaiKiemTra02/BaiKiemTra02/Models/LopHoc.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace BaiKiemTra02.Models
+{
+    public class LopHoc
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống tên lớp!")]
+        [StringLength(10, ErrorMessage = "{0} phải có độ dài phải từ {2} đến {1} ký tự.", MinimumLength = 1)]
+        [Display(Name = "Tên lớp học")]
+        public string Name { get; set; }
+
+        public DateTime ngaynhaptruong{ get; set; } = DateTime.Now;
+
+        public DateTime ngayratruong { get; set; } = DateTime.Now;
+
+        public int Soluong { get; set; }
+    }
+}
